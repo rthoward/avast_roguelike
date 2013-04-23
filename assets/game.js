385fbd3aaac9c4134ed0060f3eeb0c0f95f8578e
a-out <richbone227@gmail.com>
2013-04-23 10:33:41 -0400

    added screen capabilities to Game

--- assets/game.js
+++ assets/game.js
@@ -10,10 +10,40 @@ var Game = {
 
   init: function() {
     this._display = new ROT.Display({width: this._width, height: this._height});
+
+    var game = this;
+    var bindEventToScreen = function(event) {
+      window.addEventListener(event, function(e) {
+        if (game._currentScreen !== null) {
+          game._currentScreen.handleInput(event, e);
+        }
+      });
+    }
+    bindEventToScreen('keydown');
+    bindEventToScreen('keyup');
+    bindEventToScreen('keypress');
   },
 
   getDisplay: function() {
     return this._display;
+  },
+
+  switchScreen: function(newScreen) {
+    
+    // notify old screen of exit
+    if (this._currentScreen !== null) {
+      this._currentScreen.exit();
+    }
+
+    // clear display
+    this.getDisplay.clear();
+
+    // update screen, notify it, and render
+    this._currentScreen = newScreen;
+    if (!this._currentScreen !== null) {
+      this._currentScreen.enter();
+      this._currentScreen.render(this.getDisplay());
+    }
   }
 }
 
