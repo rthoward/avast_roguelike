aa9915a0c96388d1882f8567c90fe86b1411afc2
a-out <richbone227@gmail.com>
2013-04-23 20:46:13 +0000

    moved to entity list system

--- assets/map.js
+++ assets/map.js
@@ -1,8 +1,45 @@
-Game.Map = function(tiles) {
+Game.Map = function(tiles, player) {
   this._tiles = tiles;
-
   this._width = tiles.length;
   this._height = tiles[0].length;
+
+  // list to hold all entities
+  this._entities = [];
+
+  this._scheduler = new ROT.Scheduler.Simple();
+  this._engine = new ROT.Engine(this._scheduler);
+
+  this.addEntityAtRandomPosition(player);
+
+  // add some random fungi
+  for (var i = 0; i < 1000; i++) {
+    this.addEntityAtRandomPosition(new Game.Entity(Game.FungusTemplate));
+  }
+
+}
+
+Game.Map.prototype.addEntity = function(entity) {
+
+  // check that entity's position is within bounds
+  if (entity.getX() < 0 || entity.getX() >= this._width ||
+      entity.getY() < 0 || entity.getY() >= this._height) {
+    throw new Error('adding entity out of bounds');
+  }
+
+  entity.setMap(this);
+  this._entities.push(entity);
+
+  // if entity is an actor, add them to the scheduler
+  if (entity.hasMixin('Actor')) {
+    this._scheduler.add(entity, true);
+  }
+}
+
+Game.Map.prototype.addEntityAtRandomPosition = function(entity) {
+  var position = this.getRandomFloorPosition();
+  entity.setX(position.x);
+  entity.setY(position.y);
+  this.addEntity(entity);
 }
 
 Game.Map.prototype.getWidth = function() {
@@ -13,8 +50,8 @@ Game.Map.prototype.getHeight = function() {
   return this._height;
 }
 
+// returns the tile at the given coordinate
 Game.Map.prototype.getTile = function(x, y) {
-
   // make sure requested coord is in bounds
   if (x < 0 || x >= this._width || y < 0 || y >= this._height) {
     return Game.Tile.nullTile;
@@ -23,18 +60,34 @@ Game.Map.prototype.getTile = function(x, y) {
   }
 }
 
+Game.Map.prototype.getEngine = function()     { return this._engine; }
+Game.Map.prototype.getEntities = function()   { return this._entities; }
+
+// returns the entity at the given coordinate
+Game.Map.prototype.getEntityAt = function(x, y) {
+  for (var i = 0; i < this._entities.length; i++) {
+    if (this._entities[i].getX() == x && this._entities[i].getY() == y) {
+      return this._entities[i];
+    }
+  }
+
+  return false;
+}
+
+// digs through the entity at the given coordinate, but does not move there
 Game.Map.prototype.dig = function(x, y) {
   if (this.getTile(x, y).isDiggable()) {
     this._tiles[x][y] = Game.Tile.floorTile;
   }
 }
 
+// returns valid random floor position
 Game.Map.prototype.getRandomFloorPosition = function() {
   var x, y;
   do {
     x = Math.floor(Math.random() * this._width);
     y = Math.floor(Math.random() * this._width);
-  } while (this.getTile(x, y) != Game.Tile.floorTile);
+  } while (this.getTile(x, y) != Game.Tile.floorTile || this.getEntityAt(x, y) );
 
   return {x: x, y: y};
 }
