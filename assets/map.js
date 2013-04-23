86a26741ba04fd7a9f259847215a3c8bab029463
a-out <richbone227@gmail.com>
2013-04-23 19:42:44 +0000

    added dig() and getRandomFloorPosition()

--- assets/map.js
+++ assets/map.js
@@ -22,3 +22,19 @@ Game.Map.prototype.getTile = function(x, y) {
     return this._tiles[x][y] || Game.Tile.nullTile;
   }
 }
+
+Game.Map.prototype.dig = function(x, y) {
+  if (this.getTile(x, y).isDiggable()) {
+    this._tiles[x][y] = Game.Tile.floorTile;
+  }
+}
+
+Game.Map.prototype.getRandomFloorPosition = function() {
+  var x, y;
+  do {
+    x = Math.floor(Math.random() * this._width);
+    y = Math.floor(Math.random() * this._width);
+  } while (this.getTile(x, y) != Game.Tile.floorTile);
+
+  return {x: x, y: y};
+}
