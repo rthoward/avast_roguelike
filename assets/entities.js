a8fafa736779d4d19c467c1480dbac0997e44ae7
a-out <richbone227@gmail.com>
2013-04-23 20:44:35 +0000

    improved tryMove and added Actor mixin group

--- assets/entities.js
+++ assets/entities.js
@@ -6,8 +6,14 @@ Game.Mixins.Moveable = {
   tryMove: function(x, y, map) {
     var tile = map.getTile(x, y);
 
-    // if tile can be walked on, simply walk to it
-    if (tile.isWalkable()) {
+    // potential entity at desired location
+    var target = map.getEntityAt(x, y);
+
+    // entity is present at tile. cannot move to it
+    if (target) {
+      return false;
+      // if tile can be walked on, simply walk to it
+    } else if (tile.isWalkable()) {
       this._x = x;
       this._y = y;
       return true;
@@ -22,9 +28,33 @@ Game.Mixins.Moveable = {
   }
 }
 
+Game.Mixins.PlayerActor = {
+  name: 'PlayerActor',
+  groupName: 'Actor',
+
+  act: function() {
+    Game.refresh();
+    this.getMap().getEngine().lock();
+  }
+}
+
+Game.Mixins.FungusActor = {
+  name: 'FungusActor',
+  groupName: 'Actor',
+  act: function() {}
+}
+
+// templates -----------------------------------------------
+
 Game.PlayerTemplate = {
   character: '@',
   foreground: 'white',
   background: 'black',
-  mixins: [Game.Mixins.Moveable]
+  mixins: [Game.Mixins.Moveable, Game.Mixins.PlayerActor]
+}
+
+Game.FungusTemplate = {
+  character: 'F',
+  foreground: 'green',
+  mixins: [Game.Mixins.FungusActor]
 }
